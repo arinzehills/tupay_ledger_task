346774dc010e442f05628e2a919ef318b66a3441
arinzehills <arinzehill@gmail.com>
2026-08-03 16:29:01 +0100

    setting up swagger 1

---
+++ app/Http/Controllers/ApiDocs.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     title="Tupay Ledger & Settlement Engine",
+ *     version="1.0.0",
+ *     description="Production-grade cross-border financial backend (NGN ↔ CNY)",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     securityScheme="Bearer",
+ * )
+ */
+class ApiDocs
+{
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Get(
+ *     path="/api/user",
+ *     summary="Get the authenticated user",
+ *     tags={"User"},
+ *     security={{"Bearer":{}}},
+ *     @OA\Response(response=200, description="The authenticated user"),
+ *     @OA\Response(response=401, description="Unauthenticated")
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
